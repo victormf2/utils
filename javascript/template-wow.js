8f31862833a0acf779642e9eacadd0955ef41f92
Victor <Victor@sinforoso>
2013-10-12 18:06:14 -0300

    TemplateWow Setando valores de input, select e radio buttons

--- javascript/template-wow.js
+++ javascript/template-wow.js
@@ -1,17 +1,28 @@
+function TemplateWow() {
+}
 (function() {
-
-    function getAllChildren(el) {
-        var allChildren = [], children = el.children;
-        for (var i = 0; i < children.length; i++) {
-            allChildren.push(children[i]);
-            allChildren = allChildren.concat(getAllChildren(children[i]));
+    "use strict";
+    function mergeObjects(o1, o2) {
+        for (var attr in o2) {
+            if (!attr) {
+                continue;
+            }
+            if (typeof o1[attr] === 'object' && typeof o2[attr] === 'object') {
+                if (o1[attr] instanceof Array && o2[attr] instanceof Array) {
+                    o1[attr] = o1[attr].concat(o2[attr]);
+                } else {
+                    mergeObjects(o1[attr], o2[attr]);
+                }
+            } else {
+                o1[attr] = o2[attr];
+            }
         }
-        return allChildren;
     }
 
     function getTemplate(el) {
         var template = {};
-        return generateTemplate(el, template);
+        buildTemplate(el, template);
+        return template;
     }
 
     function getObjectFromHierarchy(hierarchy, object) {
@@ -24,24 +35,64 @@
         return object;
     }
 
-    function generateTemplate(el, obj) {
+    function buildTemplate(el, template) {
         var children = el.children;
         for (var i = 0; i < children.length; i++) {
-            var child = children[i], name = child.getAttribute('bind') || '';
+            var child = children[i], name = child.name || child.getAttribute('name');
             if (name) {
                 var hierarchy = name.split('.'),
-                    object = generateTemplate(child, obj);
-                object.__el = children[i];
+                    object = getObjectFromHierarchy(hierarchy, template),
+                    info = object[''];
+                if (info) {
+                    var element = info.element;
+                    if (element instanceof Array) {
+                        element.push(child);
+                    } else {
+                        info.element = [element, child];
+                    }
+                } else {
+                    object[''] = {element: child};
+                }
+                mergeObjects(template, object);
             }
-
-
+            buildTemplate(child, template);
         }
     }
 
-    TemplateWow = function(element) {
-        this.el = element;
-    };
-    TemplateWow.prototype.getData = function() {
+    function setDataWithTemplate(template, data) {
+        for (var attr in template) {
+            if (!attr) {
+                var element = template[attr].element, dataType = typeof data;
+                if (dataType !== 'string' && dataType !== 'number') {
+                    continue;
+                }
+                if (element instanceof HTMLInputElement || element instanceof  HTMLTextAreaElement) {
+                    element.value = data;
+                } else if (element instanceof HTMLSelectElement) {
+                    for (var option, i = 0; option = element.options[i]; i++) {
+                        if (option.value == data) {
+                            element.selectedIndex = i;
+                            break;
+                        }
+                    }
+                } else if (element instanceof Array) {
+                    for (var radio, i = 0; radio = element[i]; i++) {
+                        if (radio.value == data) {
+                            radio.checked = true;
+                            break;
+                        }
+                    }
+                }
+                continue;
+            }
+            setDataWithTemplate(template[attr], data[attr] || {});
+        }
+    }
 
+    TemplateWow.prototype.setData = function(el, data) {
+        var template = getTemplate(el);
+        setDataWithTemplate(template, data);
+    };
+    TemplateWow.prototype.getData = function(el) {
     };
 })();
