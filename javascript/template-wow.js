a43fe4246eeddecca8d31b8408f888bf90eea4b6
Victor <Victor@sinforoso>
2013-11-20 06:48:16 -0200

    Novo esquema reformulado muito louco

--- javascript/template-wow.js
+++ javascript/template-wow.js
@@ -1,11 +1,23 @@
 "use strict";
 function TemplateWow(el) {
     var _ = TemplateWow.utils,
-        template = _.getTemplate(el);
-
+        template = _.getTemplate(el),
+        dynaObj = new _.DynamicObject(template);
+    Object.defineProperty(this, 'data', {
+        enumerable: true,
+        get: function() {
+            return dynaObj;
+        },
+        set: function(data) {
+            for (var name in data) {
+                dynaObj[name] = data[name];
+            }
+        }
+    });
 }
 TemplateWow.utils = (function() {
     var utils = {};
+
     var getElementData;
     if (document.documentElement.dataset) {
         getElementData = function(el, name) {
@@ -16,6 +28,7 @@ TemplateWow.utils = (function() {
             return el.getAttribute('data-' + name);
         };
     }
+
     function mergeObjects(o1, o2) {
         for (var attr in o2) {
             if (!attr) {
@@ -72,83 +85,147 @@ TemplateWow.utils = (function() {
     function notNullUndefinedString(str) {
         return str === undefined || str === null ? '' : str;
     }
-    utils.setDataWithTemplate = function(template, data) {
-        for (var attr in template) {
-            if (!attr) {
-                var element = template[attr].element;
-                if (element instanceof HTMLInputElement) {
-                    if (element.type === 'checkbox') {
-                        element.checked = data;
-                    } else {
-                        element.value = notNullUndefinedString(data);
-                    }
-                } else if (element instanceof  HTMLTextAreaElement) {
-                    element.value = notNullUndefinedString(data);
-                } else if (element instanceof HTMLSelectElement) {
-                    for (var option, i = 0; option = element.options[i]; i++) {
-                        if (option.value == data) {
-                            element.selectedIndex = i;
-                            break;
-                        }
-                    }
-                } else if (element instanceof Array) {
-                    for (var radio, i = 0; radio = element[i]; i++) {
-                        if (radio.value == data) {
-                            radio.checked = true;
-                            break;
-                        }
-                    }
-                }
-                continue;
+    function dashToCamelCase(str) {
+        return str.replace(/-(.)/g, function(_, s) {
+            return s.toUpperCase();
+        });
+    }
+
+    function defineDynamicObjectProperty(dynaObj, name, template) {
+        Object.defineProperty(dynaObj, '_' + name, {
+            enumerable: false,
+            value: new DynamicObject(template[name])
+        });
+        Object.defineProperty(dynaObj, name, {
+            enumerable: true,
+            get: function() {
+                return dynaObj[''].get(name);
+            },
+            set: function(value) {
+                dynaObj[''].set(name, value);
             }
-            this.setDataWithTemplate(template[attr], (data || {})[attr]);
-        }
-    };
+        });
+    }
 
     function DynamicObject(template) {
+        Object.defineProperty(this, '', {
+            enumerable: false,
+            value: new DynamicObjectProperties(this)
+        });
+        if (template['']) {
+            var elementList, element = template[''].element;
+            if (element instanceof Array) {
+                elementList = element.map(function(el) {
+                    return {
+                        element: el,
+                        templater: DynamicObject.templaterForElement(el)
+                    };
+                });
+            } else {
+                elementList = [{
+                        element: element,
+                        templater: DynamicObject.templaterForElement(element)
+                    }];
+            }
+            this[''].elementList = elementList;
+            this[''].type = DynamicObject.type.value;
+            return;
+        }
+        this[''].type = DynamicObject.type.object;
         for (var name in template) {
-            Object.defineProperty(this, '_' + name, {
-                enumerable: false,
-                value: new DynamicObject(template[name])
-            });
-            Object.defineProperty(this, name, {
-                enumerable: true,
-                get: function() {
-                    return this.get(name);
-                },
-                set: function(value) {
-                    this.set(name, value);
-                }
-            });
+            defineDynamicObjectProperty(this, name, template);
         }
     }
-    DynamicObject.TYPE_VALUE = 1;
-    DynamicObject.prototype.get = function(name) {
-        return this['_' + name];
+    DynamicObject.type = {
+        object: 1,
+        value: 2
     };
-    DynamicObject.prototype.set = function(name, value) {
-        if (this.type === DynamicObject.TYPE_VALUE)
-    };
-
-    utils.createDynamicObjectWithTemplate = function(template) {
-
-        for (var name in template) {
-            obj['_' + name] = {};
+    DynamicObject.templaterForElement = function(element) {
+        if (element instanceof HTMLInputElement) {
+            if (element.type === 'checkbox') {
+                return DynamicObject.templater.checkbox;
+            } else if (element.type === 'radio') {
+                return DynamicObject.templater.radio;
+            } else {
+                return DynamicObject.templater.text;
+            }
+        } else if (element instanceof  HTMLTextAreaElement) {
+            return DynamicObject.templater.text;
+        } else if (element instanceof HTMLSelectElement) {
+            return DynamicObject.templater.select;
+        } else {
+            return DynamicObject.templater.html;
         }
     };
+    DynamicObject.templater = {
+        text: {
+            render: function(value) {
+                this.value = notNullUndefinedString(value);
+            }
+        },
+        checkbox: {
+            render: function(value) {
+                this.checked = value;
+            }
+        },
+        select: {
+            render: function(value) {
+                for (var option, i = 0; option = this.options[i]; i++) {
+                    if (option.value == value) {
+                        this.selectedIndex = i;
+                        break;
+                    }
+                }
+            }
+        },
+        radio: {
+            render: function(value) {
+                this.checked = this.value == value;
+            }
+        },
+        html: {
+            render: function(value) {
+                this.innerHTML = value;
+            }
+        }
+    };
+    function DynamicObjectProperties(dynaObj) {
+        this.dynaObj = dynaObj;
+    }
+    DynamicObjectProperties.prototype.get = function(name) {
+        var prop = this.dynaObj['_' + name];
+        return prop[''].isValue() ? prop[''].value : prop;
+    };
+    DynamicObjectProperties.prototype.set = function(name, value) {
+        var prop = this.dynaObj['_' + name];
+        if (prop[''].isValue()) {
+            prop[''].value = value;
+            prop[''].elementList.forEach(function(item) {
+                item.templater.render.call(item.element, value);
+            });
+            return;
+        }
+        for (var n in value) {
+            prop[n] = value[n];
+        }
+    };
+    DynamicObjectProperties.prototype.isValue = function() {
+        return this.type === DynamicObject.type.value;
+    };
+    DynamicObjectProperties.prototype.asSimpleObject = function() {
+        var obj = {}, value;
+        for (var name in this.dynaObj) {
+            value = this.dynaObj[name];
+            obj[name] = value instanceof DynamicObject ? value[''].asSimpleObject() : value;
+        }
+        return obj;
+    };
+
+    utils.DynamicObject = DynamicObject;
+
     return utils;
 })();
 
-TemplateWow.prototype.setData = function(el, data) {
-    var _ = TemplateWow.utils;
-    var template = _.getTemplate(el);
-    _.setDataWithTemplate(template, data);
-};
-
-TemplateWow.prototype.bindData = function(el, data) {
-    var template = TemplateWow.utils.getTemplate(el);
-};
-
-
-TemplateWow.prototype.getData = function(el) {
+TemplateWow.prototype.simpleObject = function() {
+    return this.data[''].asSimpleObject();
 };
